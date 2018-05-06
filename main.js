feca1ab1a2d304dbd246fb3d6640f68c3dc7128f
Ambler <allinlbj@gmail.com>
2018-05-06 23:57:23 +0800

    「进一步的优化，v2.0」

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-$('.images > img:nth-child(4)').addClass('enter')
-$('.images > img:nth-child(5)').addClass('enter')
-
-let n = 1
+let n
+init()
 setInterval(() => {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(e.currentTarget))
         })
-    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n + 1))
     n += 1
 }, 3000)
+
+
+
+// 以下就不用看了
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
 
 function x(n) {
     if (n > 5) {
@@ -20,6 +22,22 @@ function x(n) {
         if (n === 0) {
             n = 5
         }
-    }
+    } // n = 1 2 3
     return n
+}
+
+function init() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
 }
